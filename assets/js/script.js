cc56187c06dfa4fc2766004deabf5922d004d741
Steven Phan <stevenphan@msn.com>
2022-01-05 05:46:05 +1100

    Separated automatic and manual save functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,25 @@
 // keep input field in focus and ...
 document.querySelector("textarea").focus();
 // ... display lasted saved note in the saved window
-document.getElementById("last-saved-note").textContent = localStorage.getItem("saved-document");
+document.getElementById("manually-saved-note").textContent = localStorage.getItem("manually-saved-document");
+document.getElementById("automatically-saved-note").textContent = localStorage.getItem("automatically-saved-document");
 
 // save button
 var saveButton = document.getElementById("save-button");
 saveButton.addEventListener("click", function(event) {
     // save the document in local storage
-    localStorage.setItem("saved-document", document.getElementById("current-note").value);
+    localStorage.setItem("manually-saved-document", document.getElementById("current-note").value);
     // display content in the saved window
-    document.getElementById("last-saved-note").textContent = localStorage.getItem("saved-document");
+    document.getElementById("manually-saved-note").textContent = localStorage.getItem("manually-saved-document");
     // return focus to input field
     document.querySelector("textarea").focus();
 });
 
-// restore button
-var restoreButton = document.getElementById("restore-button");
-restoreButton.addEventListener("click", function(event) {
+// restore manual button
+var restoreManualButton = document.getElementById("restore-manual-button");
+restoreManualButton.addEventListener("click", function(event) {
     // replace content in the input field with the lasted saved note
-    document.getElementById("current-note").value = localStorage.getItem("saved-document");
+    document.getElementById("current-note").value = localStorage.getItem("manually-saved-document");
     // return focus to input field
     document.querySelector("textarea").focus();
 });
@@ -29,22 +30,21 @@ restoreButton.addEventListener("click", function(event) {
 var clearButton = document.getElementById("clear-button");
 clearButton.addEventListener("click", function(event) {
     // removed lasted saved note from local stoage
-    localStorage.removeItem("saved-document");
+    localStorage.removeItem("manually-saved-document");
     console.log("hi!");
     // update the history display window
-    document.getElementById("last-saved-note").textContent = localStorage.getItem("saved-document");
+    document.getElementById("manually-saved-note").textContent = localStorage.getItem("manually-saved-document");
     // return focus to input field
     document.querySelector("textarea").focus();
 });
 
 
 // Save every minute
-// Need to edit so not repeated...
 setInterval(function(event) {
     // save the document in local storage
-    localStorage.setItem("saved-document", document.getElementById("current-note").value);
+    localStorage.setItem("automatically-saved-document", document.getElementById("current-note").value);
     // display content in the saved window
-    document.getElementById("last-saved-note").textContent = localStorage.getItem("saved-document");
+    document.getElementById("automatically-saved-note").textContent = localStorage.getItem("automatically-saved-document");
     // return focus to input field
     document.querySelector("textarea").focus();
-}, 60000);
+}, 10000);
